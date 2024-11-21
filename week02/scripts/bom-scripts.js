d1664470aa9bcf16b58cc4d88650f6ea766b5862
jyrobacus <bacusjyro@gmail.com>
2024-11-21 20:01:11 +0400

    changes

--- week02/scripts/bom-scripts.js
+++ week02/scripts/bom-scripts.js
@@ -3,9 +3,6 @@ const button = document.querySelector('button')
 const list = document.querySelector('#list')
 const li = document.createElement('li');
 const deleteButton = document.createElement('button');
-li.textContent = input.value;
-deleteButton.textContent = '❌';
-li.append(deleteButton);
 list.append(li);
 button.addEventListener('click', function() {
     if (input.value.trim() !== "") {
