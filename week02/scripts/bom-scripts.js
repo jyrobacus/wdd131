83bea032b87c94e9116ef9cd3a0fa877f61e2ff5
jyrobacus <bacusjyro@gmail.com>
2024-11-21 19:59:42 +0400

    added javascript

---
+++ week02/scripts/bom-scripts.js
@@ -0,0 +1,27 @@
+const input = document.querySelector('#favchap')
+const button = document.querySelector('button')
+const list = document.querySelector('#list')
+const li = document.createElement('li');
+const deleteButton = document.createElement('button');
+li.textContent = input.value;
+deleteButton.textContent = '❌';
+li.append(deleteButton);
+list.append(li);
+button.addEventListener('click', function() {
+    if (input.value.trim() !== "") {
+        li.textContent = input.value;
+        deleteButton.textContent = "❌";
+        li.append(deleteButton);
+        list.append(li);
+        input.value = "";
+    }
+      input.focus();
+    });
+    
+    deleteButton.addEventListener("click", function () {
+      list.removeChild(li);
+      input.focus();
+    });
+
+
+
